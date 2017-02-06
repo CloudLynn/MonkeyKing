b8468b5bafe8871c08beee98573ddadf006d9fed
xianjl <15328539380@163.com>
2017-02-06 12:17:42 +0800

    Initial Commit

---
+++ MonkeyKing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MonkeyKing
+//
+//  Created by Apple on 2017/2/6.
+//  Copyright © 2017年 Shengwukong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MonkeyKing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MonkeyKing
+//
+//  Created by Apple on 2017/2/6.
+//  Copyright © 2017年 Shengwukong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
